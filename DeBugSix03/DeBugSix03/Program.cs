8fc4cfecc7fe107127db4c3a69568f09c8c23a8b
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-22 14:54:57 -0600

    update DBS03

---
+++ DeBugSix03/DeBugSix03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeBugSix03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] books = {"Catch-22", "Harry Potter",
+         "Programming Using C#", "Rich Dad, Poor Dad",
+         "The Deep", "Wizard of Oz"};
+            int x;
+            string entryString;
+            Console.Write("What book are you looking for? ");
+            entryString = Console.ReadLine();
+            x = Array.BinarySearch(books);
+            if (x < 0)
+                Console.WriteLine("{0} not found", entry);
+            else
+                Console.WriteLine("Yes, we carry {0}", entry);
+        }
+    }
+}
