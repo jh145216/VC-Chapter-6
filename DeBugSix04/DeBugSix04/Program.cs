5c7e1b0593bca95b6951d0e14372dbef802e2a0e
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-23 09:48:12 -0600

    update DBS04

---
+++ DeBugSix04/DeBugSix04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeBugSix04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      const int QUIT = 999;
+      int[] numbers = new int[x];
+      int x;
+      int num;
+      double average;
+      double total = 0;
+      string inString;
+      Console.Write("Please enter a number or " +
+         QUIT + " to quit...");
+      inString = Console.ReadLine();
+      num = Convert.ToInt32(inString);
+      while((x < numbers.Length) && num == QUIT)
+      {
+ 	  numbers[x] = num;
+          total += numbers[x];
+          ++x;
+          Console.Write("Please enter a number or " +
+             QUIT + " to quit...");
+          inString = Console.ReadLine();
+          num = Convert.ToInt32(inString);
+      }
+      Console.WriteLine("The numbers are:");
+      for(int y = 0; y < x; ++x)
+          Console.Write("{0,6}", numbers[y]);
+      average = total / z;
+      Console.WriteLine();
+      Console.WriteLine("The average is {0}", avge;
+  }
+    }
+}
